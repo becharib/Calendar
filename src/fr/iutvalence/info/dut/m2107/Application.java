17f9acf3b6d92e1bd90fed9b837017a7d25d16a0
Charlotte Destrait <charlotte.destrait@iut-valence.fr>
2016-05-24 18:54:49 +0200

    Creation of the Main and start to show days.

--- src/fr/iutvalence/info/dut/m2107/Application.java
+++ src/fr/iutvalence/info/dut/m2107/Application.java
@@ -1,14 +1,25 @@
 package fr.iutvalence.info.dut.m2107;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 public class Application {
 	
 	public static void main(String[] args){
-		
-	}
 
-	public Calendrier consulterCalendrier() {
-		// TODO - implement Application.consulterCalendrier
-		throw new UnsupportedOperationException();
+		Scanner sc1 = new Scanner(System.in);
+		
+		System.out.printf("Year ? Exemple : 2016 \n");
+		String yearString = sc1.nextLine();
+		int year = Integer.parseInt(yearString); 
+		
+		System.out.printf("Month ? Exemple : July \n");
+
+		Scanner sc2 = new Scanner(System.in);
+		String month = sc2.nextLine();
+		
+		System.out.printf("[%s,%d]\n", month, year);
+		new Month(month, year);
 	}
 
 }
--- src/fr/iutvalence/info/dut/m2107/Month.java
+++ src/fr/iutvalence/info/dut/m2107/Month.java
@@ -7,85 +7,85 @@ public class Month {
 	private int num;
 	private Date[] month;
 	
-	public Month(String ActualMonth, int annee){
+	public Month(String ActualMonth, int year){
 		int k = 1;
 		this.name=ActualMonth;
-		this.year=annee;
+		this.year=year;
 		
 		switch (ActualMonth)
 		{
 		  case "January":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=1;
 		    break;
 		  case "February":
 			  this.day=29;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=2;
 		    break;
 		  case "March":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=3;
 		    break;
 		  case "April":
 			  this.day=30;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=4;
 			  break;
 		  case "May":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=5;
 		    break;
 		  case "June":
 			  this.day=30;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=6;
 			  break;
 		  case "July":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=7;
 		    break;
 		  case "August":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=8;
 		    break;
 		    
 		  case "September":
 			  this.day=30;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=9;
 		    break;
 		  case "October":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=10;
 		    break;
 		  case "November":
 			  this.day=30;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=11;
 		    break;
 		  case "December":
 			  this.day=31;
-			  this.month = new Date[this.day];
+			  this.month = new Date[this.day+1];
 			  this.num=12;
 		    break;
 		}
 		
 		
-		for(int i=1;i<this.day;i++){
+		for(int i=1;i<=this.day;i++){
 			month[i]= new Date(i,this.num,this.year);
-			System.out.println(month[i]);
+			System.out.println("Day " + i + "[Event:  ]");
 		}
 		
 		
 	}
-	//à compléter
+	//ï¿½ complï¿½ter
 
 	 
 	/*January,                 	
