b78bb79ed270f3b1e0d9fa6cf838d418a73e3a9d
Charlotte Destrait <charlotte.destrait@iut-valence.fr>
2016-05-24 11:01:54 +0200

    window

--- src/fr/iutvalence/info/dut/m2107/Window.java
+++ src/fr/iutvalence/info/dut/m2107/Window.java
@@ -1,5 +1,9 @@
 package fr.iutvalence.info.dut.m2107;
+import javax.swing.JFrame;
 
 public class Window {
-	
+	public static void main(String[] args){
+	    JFrame fenetre = new JFrame();
+	    fenetre.setVisible(true);
+	  } 
 }
