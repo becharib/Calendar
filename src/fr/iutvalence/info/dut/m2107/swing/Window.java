80c88a8c29e0a2ed4a2743774ead35e2f4175636
becharib <becharib@PC-B122-07.iut-valence.fr>
2016-05-24 18:24:00 +0200

    Initialisation of Interfaces

--- src/fr/iutvalence/info/dut/m2107/swing/Window.java
+++ src/fr/iutvalence/info/dut/m2107/swing/Window.java
@@ -1,11 +1,24 @@
 package fr.iutvalence.info.dut.m2107.swing;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class Window {
+public class Window extends JFrame{
 	
-	public static void main(String[] args){
-	    JFrame fenetre = new JFrame();
-	    fenetre.setVisible(true);
-	  } 
+	public static void main(String[] args) {
+		
+		Window window = new Window();		
+	    window.setTitle("Calendar");
+	    window.setSize(1000, 800);
+	    window.setLocationRelativeTo(null);
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
+	    window.setVisible(true);
+	    ImageIcon img = new ImageIcon("img/calendar-icon.png");
+		window.setIconImage(img.getImage());
+		window.setResizable(false);
+		window.getContentPane().setBackground(Color.ORANGE);
+	}
+	
 }
