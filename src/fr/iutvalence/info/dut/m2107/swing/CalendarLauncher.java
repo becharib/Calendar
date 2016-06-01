3baf60f65176e44b4016947dd84f89b95cb5f791
becharib <becharib@PC-A213-07.iut-valence.fr>
2016-06-01 17:56:48 +0200

    Creation of MonthFrame class

--- src/fr/iutvalence/info/dut/m2107/swing/CalendarLauncher.java
+++ src/fr/iutvalence/info/dut/m2107/swing/CalendarLauncher.java
@@ -3,6 +3,7 @@ package fr.iutvalence.info.dut.m2107.swing;
 public class CalendarLauncher {
 	
     public static void main(String[] args) {
-    	new MainFrame();
+    	//new MainFrame();
+    	
     }
 }
---
+++ src/fr/iutvalence/info/dut/m2107/swing/MonthFrame.java
@@ -0,0 +1,8 @@
+package fr.iutvalence.info.dut.m2107.swing;
+
+import javax.swing.JFrame;
+
+public class MonthFrame extends JFrame {
+	
+	
+}
