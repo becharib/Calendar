dad76ef7df8e5fa71d90392dd91810ddc456edaa
becharib <becharib@PC-A105-13.iut-valence.fr>
2016-06-01 18:28:05 +0200

    Implements MonthFrame class

--- src/fr/iutvalence/info/dut/m2107/swing/MonthFrame.java
+++ src/fr/iutvalence/info/dut/m2107/swing/MonthFrame.java
@@ -1,8 +1,28 @@
 package fr.iutvalence.info.dut.m2107.swing;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MonthFrame extends JFrame {
-	
+
+	public MonthFrame(String jours){
+		
+		this.setContentPane(new ImagePanel(new ImageIcon("img/fond.png").getImage()));
+		this.setTitle("Calendar");
+		this.setSize(new Dimension(1000,800));
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setVisible(true);
+		this.setLayout(null);
+		
+	    for(int i=1; i<=Integer(jours); i++ ){
+	    	
+	    }
+	    
+	}
 	
 }
